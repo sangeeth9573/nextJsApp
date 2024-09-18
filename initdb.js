ee89058b29975839c4bf306d21e0258dded8e804
tecra <17b65a0104@nmrec.edu.in>
2024-09-18 19:32:17 +0530

    mongodb added

--- initdb.js
+++ initdb.js
@@ -1,5 +1,3 @@
-const sql = require('better-sqlite3');
-const db = sql('meals.db');
 
 const dummyMeals = [
   {
@@ -164,36 +162,3 @@ const dummyMeals = [
   },
 ];
 
-db.prepare(`
-   CREATE TABLE IF NOT EXISTS meals (
-       id INTEGER PRIMARY KEY AUTOINCREMENT,
-       slug TEXT NOT NULL UNIQUE,
-       title TEXT NOT NULL,
-       image TEXT NOT NULL,
-       summary TEXT NOT NULL,
-       instructions TEXT NOT NULL,
-       creator TEXT NOT NULL,
-       creator_email TEXT NOT NULL
-    )
-`).run();
-
-async function initData() {
-  const stmt = db.prepare(`
-      INSERT INTO meals VALUES (
-         null,
-         @slug,
-         @title,
-         @image,
-         @summary,
-         @instructions,
-         @creator,
-         @creator_email
-      )
-   `);
-
-  for (const meal of dummyMeals) {
-    stmt.run(meal);
-  }
-}
-
-initData();
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['zps36re6j3v99dcn.public.blob.vercel-storage.com'], // Add the domains you want to allow
+      remotePatterns: ['zps36re6j3v99dcn.public.blob.vercel-storage.com'], // Add the domains you want to allow
       },
 };
 
